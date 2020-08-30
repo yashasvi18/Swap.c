b16c6e6d9699f63f0b3e23af870c9d1ac63132c7
yashasvi18 <47742064+yashasvi18@users.noreply.github.com>
2020-08-30 12:29:57 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int a,b,temp;
+
+void change(int x,int y)
+{//swapping code
+    temp=x;
+    x=y;
+    y=temp;
+    printf("after swapping-a: %d ,b: %d",x,y);
+}
+
+int main()
+{
+
+    printf("Enetr two numbers :");
+    scanf("%d, %d",&a,&b);
+    printf("a:%d , b:%d\n",a,b);
+    change(a,b);
+
+}
